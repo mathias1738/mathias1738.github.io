691bf85483e123019d30ea7a8272d1fc108a11d6
mathias1738 <90278057+mathias1738@users.noreply.github.com>
2021-10-04 13:54:46 -0400

    Create amis.js

---
+++ amis.js
@@ -0,0 +1,25 @@
+var tblNom = ["mathias", "luke", "mihai", "timmy", "jean"];
+var joue = "oui"
+var i
+var meilleure = ("")
+
+alert("voici ma liste d'amis: " + tblNom)
+
+while (joue == "oui") {
+  var nom = prompt("Ecris un nom pour ajouter a la liste:")
+  tblNom.pop();
+  tblNom.unshift(nom);
+  alert("voici ma nouvelle liste d'amis: " + tblNom)
+
+  for (i = 0; i < tblNom.length; i++) {
+    if (i % 2 != 0) {
+      meilleure = meilleure + tblNom[i]
+    }
+    if (i == 1) {
+      meilleure = meilleure + (" et ")
+    }
+  }
+  alert("Mes meilleures amis sont: " + meilleure)
+  joue = prompt("Voulez vous continuer? (oui/non) : ")
+  meilleure = ("")
+}
